61cad59c3952e20079d88112cc2bb1c64333c2d7
Jae Proctor <jproctor@mtholyoke.edu>
2023-08-31 13:34:39 -0400

    Remove some unnecessary compounds

--- dist/lits_theme_scripts.js
+++ dist/lits_theme_scripts.js
@@ -1,62 +1,6 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ([
 /* 0 */
-/***/ (function() {
-
-// UNCOMMENT IF DRUPAL - see components/_meta/_01-foot.twig for attachBehaviors
-// Drupal.behaviors.accordion = {
-//   attach: function (context, settings) {
-
-(() => {
-  // REMOVE IF DRUPAL
-
-  // Set 'document' to 'context' if Drupal
-  const accordionTerm = document.querySelectorAll(".accordion-term");
-  const accordionDef = document.querySelectorAll(".accordion-def");
-
-  // If javascript, hide accordion definition on load
-  function jsCheck() {
-    for (let i = 0; i < accordionDef.length; i++) {
-      if (accordionDef[i].classList) {
-        accordionDef[i].classList.add("active");
-        accordionDef[0].previousElementSibling.classList.add("is-active");
-      } else {
-        accordionDef[i].className += " active";
-        accordionDef[0].previousElementSibling.classList.add("is-active");
-      }
-    }
-  }
-  jsCheck();
-
-  // Accordion Toggle
-  // Mobile Click Menu Transition
-  for (let i = 0; i < accordionTerm.length; i++) {
-    accordionTerm[i].addEventListener("click", e => {
-      const className = "is-active";
-      // Add is-active class
-      if (this.classList) {
-        this.classList.toggle(className);
-      } else {
-        const classes = this.className.split(" ");
-        const existingIndex = classes.indexOf(className);
-        if (existingIndex >= 0) {
-          classes.splice(existingIndex, 1);
-        } else {
-          classes.push(className);
-        }
-        this.className = classes.join(" ");
-      }
-      e.preventDefault();
-    });
-  }
-})(); // REMOVE IF DRUPAL
-
-// UNCOMMENT IF DRUPAL
-//   }
-// };
-
-/***/ }),
-/* 1 */
 /***/ (() => {
 
 (() => {
@@ -110,7 +54,7 @@
 })();
 
 /***/ }),
-/* 2 */
+/* 1 */
 /***/ (() => {
 
 /**
@@ -154,7 +98,7 @@
 })(jQuery, Drupal);
 
 /***/ }),
-/* 3 */
+/* 2 */
 /***/ (() => {
 
 /**
@@ -223,7 +167,7 @@
 })(jQuery, Drupal);
 
 /***/ }),
-/* 4 */
+/* 3 */
 /***/ (() => {
 
 /**
@@ -426,7 +370,7 @@
 })(jQuery, Drupal);
 
 /***/ }),
-/* 5 */
+/* 4 */
 /***/ (function() {
 
 /**
@@ -695,14 +639,13 @@
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
-/******/ 	// This entry module is referenced by other modules so it can't be inlined
 /******/ 	__webpack_modules__[0]();
 /******/ 	__webpack_modules__[1]();
 /******/ 	__webpack_modules__[2]();
 /******/ 	__webpack_modules__[3]();
-/******/ 	__webpack_modules__[4]();
+/******/ 	// This entry module is referenced by other modules so it can't be inlined
 /******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__[5]();
+/******/ 	__webpack_modules__[4]();
 /******/ 	
 /******/ })()
 ;
