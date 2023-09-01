affe4864f52559d024a66e6780c432a894448008
Jae Proctor <jproctor@mtholyoke.edu>
2023-09-01 12:14:51 -0400

    Remove unused tabs.js

--- dist/lits_theme.js
+++ dist/lits_theme.js
@@ -3,60 +3,6 @@
 /* 0 */
 /***/ (() => {
 
-(() => {
-  const el = document.querySelectorAll(".tabs");
-  const tabNavigationLinks = document.querySelectorAll(".tabs__link");
-  const tabContentContainers = document.querySelectorAll(".tabs__tab");
-  let activeIndex = 0;
-
-  /**
-   * goToTab
-   * @description Goes to a specific tab based on index. Returns nothing.
-   * @param {Number} index The index of the tab to go to
-   */
-  const goToTab = index => {
-    if (index !== activeIndex && index >= 0 && index <= tabNavigationLinks.length) {
-      tabNavigationLinks[activeIndex].classList.remove("is-active");
-      tabNavigationLinks[index].classList.add("is-active");
-      tabContentContainers[activeIndex].classList.remove("is-active");
-      tabContentContainers[index].classList.add("is-active");
-      activeIndex = index;
-    }
-  };
-
-  /**
-   * handleClick
-   * @description Handles click event listeners on each of the links in the
-   *   tab navigation. Returns nothing.
-   * @param {HTMLElement} link The link to listen for events on
-   * @param {Number} index The index of that link
-   */
-  const handleClick = (link, index) => {
-    link.addEventListener("click", e => {
-      e.preventDefault();
-      goToTab(index);
-    });
-  };
-
-  /**
-   * init
-   * @description Initializes the component by removing the no-js class from
-   *   the component, and attaching event listeners to each of the nav items.
-   *   Returns nothing.
-   */
-  for (let e = 0; e < el.length; e++) {
-    el[e].classList.remove("no-js");
-  }
-  for (let i = 0; i < tabNavigationLinks.length; i++) {
-    const link = tabNavigationLinks[i];
-    handleClick(link, i);
-  }
-})();
-
-/***/ }),
-/* 1 */
-/***/ (() => {
-
 /**
  * @file
  * Functions and behaviors for the system status page
@@ -98,7 +44,7 @@
 })(jQuery, Drupal);
 
 /***/ }),
-/* 2 */
+/* 1 */
 /***/ (() => {
 
 /**
@@ -167,7 +113,7 @@
 })(jQuery, Drupal);
 
 /***/ }),
-/* 3 */
+/* 2 */
 /***/ (() => {
 
 /**
@@ -370,7 +316,7 @@
 })(jQuery, Drupal);
 
 /***/ }),
-/* 4 */
+/* 3 */
 /***/ (function() {
 
 /**
@@ -642,10 +588,9 @@
 /******/ 	__webpack_modules__[0]();
 /******/ 	__webpack_modules__[1]();
 /******/ 	__webpack_modules__[2]();
-/******/ 	__webpack_modules__[3]();
 /******/ 	// This entry module is referenced by other modules so it can't be inlined
 /******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__[4]();
+/******/ 	__webpack_modules__[3]();
 /******/ 	
 /******/ })()
 ;
